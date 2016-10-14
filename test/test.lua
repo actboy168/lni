59e4375b874b8fa70cdd430a6247717c0733b831
actboy168 <actboy168@gmail.com>
2016-10-14 14:35:44 +0800

    增加几个测试

---
+++ test/test.lua
@@ -0,0 +1,86 @@
+package.cpath = package.cpath .. [[;.\..\bin\Debug\?.dll]]
+
+local lni = require 'lni-c'
+
+local function EQUAL(a, b)
+	for k, v in pairs(a) do
+		if type(v) == 'table' then
+			EQUAL(v, b[k])
+		else
+			assert(v == b[k], v)
+		end
+	end
+end
+
+local function TEST(script, t)
+	local r = lni(script)
+	EQUAL(r, t)
+	EQUAL(t, r)
+end
+
+TEST(
+[==[
+[ABC]  
+a = 'Hello'  
+b = 1.0  
+c = {'1', '2', '3'}  
+'d' = {  
+  x = 2,  
+  y = 4,  
+}  
+10 =  
+[[ | H  
+   | e  
+   | l  
+   | l  
+   | o  
+]]  
+]==]
+,
+{  
+ABC = {  
+a = 'Hello',  
+b = 1.0,  
+c = {'1', '2', '3'},  
+d = { x = 2, y = 4 },  
+[10] =  
+[[ | H  
+   | e  
+   | l  
+   | l  
+   | o  
+]]  
+  }  
+}  
+)
+
+TEST([==[
+[enum]  
+YES = 1  
+NO = 0  
+[A]  
+a = YES  
+b = NO  
+]==]
+,
+{
+ A = { a = 1, b = 0 }  
+}
+)
+
+
+TEST([==[
+[A]  
+a = 1  
+b = 2  
+[B:A]  
+c = 3 
+]==]
+,
+{  
+  A = { a = 1, b = 2 },  
+  B = { a = 1, b = 2, c = 3 },  
+}  
+)
+ 
+print('test ok!')
